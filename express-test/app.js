ce4e40ad06d6815d2710b6710572a9dc3ab83264
ryan.yang <ryan.yang@104.com.tw>
2023-04-04 14:53:35 +0800

    [練習] params, query

--- express-test/app.js
+++ express-test/app.js
@@ -2,13 +2,28 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    console.log(req)
     res.send(
         `
         <head>
         </head>
         <body>
             <h1>hi~</h1>
+        </body>
+        `
+    )
+})
+
+app.get('/user/:name/:date', (req, res) => {
+    const { name, date } = req.params
+    const { limit } = req.query
+    res.send(
+        `
+        <head>
+        </head>
+        <body>
+            <div>name: ${name}</div>
+            <div>date: ${date}</div>
+            <div>limit: ${limit}</div>
         </body>
         `
     )
